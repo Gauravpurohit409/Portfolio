f24d5fd76fa23ec25ba8d36ef0a726dd3a966c72
Gaurav Purohi <gauravpurohit409@gmail.com>
2019-12-07 08:47:53 +0530

    Home page is ready

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+#Create A Blog models
+	#title
+	#publication date 
+	#body
+	#image
+class Blog(models.Model):
+	Title = models.CharField(max_length = 20)
+	pub_date = models.DateTimeField()
+	body = models.TextField()
+	image = models.ImageField(upload_to = 'images/')
+
+
+#Add the Blog app to the settings
+# Create a migration
+#Migrate 
+#Add to their admin
