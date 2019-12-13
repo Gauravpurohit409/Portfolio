6b45510c1fee14a7668af15d1a04fc382054801e
Gaurav Purohi <gauravpurohit409@gmail.com>
2019-12-13 23:21:19 +0530

    Ready to Live

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
+from  .models import Blog
 # Create your views here.
 
-def articles(request):
+def allblogs(request):
+	blogs = Blog.objects
+	return render(request,'blog/allblogs.html',{'blogs' : blogs}) 
 
-	return render(request,'blog/blog.html')
+def detail(request,blog_id):
+	detailblog = get_object_or_404(Blog,pk = blog_id)
+	return render(request,'blog/detail.html',{'blog' : detailblog})
+
